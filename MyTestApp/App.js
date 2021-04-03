520d5bd2114d3230ca0a8dd99f07af83e867a7a2
ilhamfakihhilmi <67012067+ilhamfakihhilmi@users.noreply.github.com>
2021-04-03 19:39:42 +0700

    app
    basic styling react native

--- MyTestApp/App.js
+++ MyTestApp/App.js
@@ -1,7 +1,58 @@
 import React, { Component } from 'react';
-import { Text, View, Image, TextInput } from 'react-native';
+import { Text, View, Image, TextInput, StyleSheet } from 'react-native';
+import legion from '../MyTestApp/img/legion.jpg'
 
 const App = () => {
+  return (
+    <StylingReactNativeComponent />
+  )
+}
+
+const StylingReactNativeComponent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Styling Component</Text>
+      <View style={{
+        width: 100,
+        height: 100,
+        backgroundColor: '#0abde3',
+        borderWidth: 2,
+        borderColor: 'red',
+        marginTop: 20,
+        marginLeft: 20,
+      }} 
+      />
+    <View style={{padding:12, backgroundColor: '#f2f2f2', width:212, borderRadius: 10}}>
+      <Image source={legion} style={{ width: 188, height: 107, borderRadius: 8}} />
+      <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>Legion 5i i7 10750H</Text>
+      <Text style={{fontSize: 12,color: '#f2994a', fontWeight: 'bold', marginTop: 16}}>Rp. 21.350.000,-</Text>
+      <Text style={{fontSize: 12,color: '#000000', fontWeight: 'normal', marginTop: 16}}>Bekasi</Text>
+      <View style={{backgroundColor: '#6fcf97', borderRadius: 20, paddingVertical: 6, marginTop: 20 }}>
+        <Text 
+        style={{color: '#ffffff', fontWeight: '600', textAlign: 'center',fontSize: 14}}>BELI</Text>
+      </View>
+    </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'green',
+    marginLeft: 20,
+    marginTop: 20,
+  }
+})
+
+
+
+
+
+
+
+const SampleComponent = () => {
   return (
     <View>
       <View
